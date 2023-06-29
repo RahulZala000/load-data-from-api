f1bd8d9ba736926ddbab642ef9a78f18a70c4fcb
RahulZ <rahulmz@zignuts.com>
2023-06-29 12:20:52 +0530

    api with recycleview and data model:

--- app/src/main/java/com/example/api_demo/CustomAdapter.kt
+++ app/src/main/java/com/example/api_demo/CustomAdapter.kt
@@ -6,19 +6,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter (private var dataList: List<ViewModel>): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter(private var dataList: List<MydataItem>): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
     {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val ItemsViewModel = mList[position]
+        val ItemsViewModel = dataList.get(position)
         holder.textView.text = ItemsViewModel.toString()
     }
 
     override fun getItemCount(): Int {
-        return mList.size
+        return dataList.size
     }
 
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
--- app/src/main/java/com/example/api_demo/MainActivity.kt
+++ app/src/main/java/com/example/api_demo/MainActivity.kt
@@ -1,45 +1,29 @@
 package com.example.api_demo
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.ResponseDelivery
-import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.example.api_demo.databinding.ActivityMainBinding
-import org.json.JSONArray
-import org.json.JSONException
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.StringBuilder
-import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    var dataList = ArrayList < MydataItem > ()
-    lateinit var recyclerView: RecyclerView
+    var dataList:List<MydataItem>? =  ArrayList()
     var myAdapter: CustomAdapter ? = null
     val Base="https://jsonplaceholder.typicode.com/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        recyclerView = findViewById(R.id.reuse)
-        myAdapter = CustomAdapter(ViewModel, this)
-        recyclerView.adapter = myAdapter
-
 
         getData()
 
@@ -56,9 +40,9 @@ class MainActivity : AppCompatActivity() {
         retroData.enqueue(object : Callback<List<MydataItem>?>{
             override fun onResponse(call: Call<List<MydataItem>?>, response: retrofit2.Response<List<MydataItem>?>)
             {
-                var Res = response.body()
+                dataList = response.body()
                 var mystr= StringBuilder()
-                if (Res != null) {
+               /* if (Res != null) {
                     for (Mydata in Res)
                     {
                         mystr.append(Mydata.id)
@@ -66,13 +50,17 @@ class MainActivity : AppCompatActivity() {
                         mystr.append(Mydata.title)
                         mystr.append(" ")
                        mystr.append("\n")
-                      /*  if(Mydata.id%10==0)
-                            mystr.append("\n")*/
+
+                      *//*  if(Mydata.id%10==0)
+                            mystr.append("\n")*//*
 
                     }
-                }
+                }*/
+                myAdapter= CustomAdapter(dataList = dataList!!)
+                binding.reuse.layoutManager=LinearLayoutManager(this@MainActivity)
+                binding.reuse.adapter=myAdapter
 
-                binding.ans.text=mystr
+              //  binding.ans.text=mystr
             }
 
             override fun onFailure(call: Call<List<MydataItem>?>, t: Throwable) {
